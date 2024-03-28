fce3f0f0cbc3940d5fb404302c3e144a234f8251
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-03-28 18:19:08 -0300

    feat setup global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { Inter } from '@next/font/google'
 import type { AppProps } from 'next/app'
+import { GlobalStyles } from '../../styles/Global'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={inter.className}>
-        
       <Component {...pageProps}  />
-    </div>)
-  
+      <GlobalStyles />   
+    </div>
+  )
 }
