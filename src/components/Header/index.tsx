4cb7fe5bbd64d32899511c1844c961c653c652ed
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-03-28 18:36:10 -0300

    feat header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import * as S from './styles'
 import PokeballIcon from '../../../public/images/white-pokeball.svg'
+import Link from 'next/link'
 
 export function Header() {
   return (
@@ -9,6 +10,10 @@ export function Header() {
         <Image src={PokeballIcon} alt='pokeball' width={37} height={37} />
         <span>Centro Pokémon</span>
       </S.HomeLink>
+      <S.Navigation>
+        <S.OutlineLink href={'#'}>Quem somos</S.OutlineLink>
+        <S.ScheduleLink href={'#'}>Agendar Consulta</S.ScheduleLink>
+      </S.Navigation>
     </S.Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: space-between;
   padding: 1.25rem 5.188rem;
-  background-color: #dddedd;
+  background-color: #FFF;
 `
 
 export const HomeLink = styled(Link)`
@@ -29,4 +31,26 @@ export const HomeLink = styled(Link)`
   &:hover {
     width: 259px;
   }
+`
+
+export const Navigation = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 30px;
+`
+
+export const OutlineLink = styled(Link)`
+  color: rgba(0, 0, 0, 1);
+  text-decoration: none;
+  text-transform: capitalize;
+`
+
+export const ScheduleLink = styled(Link)`
+  padding: 0.75rem;
+  border-radius: 50px;
+  background-color: rgba(228, 15, 15, 1);
+  text-decoration: none;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 700;    
 `
