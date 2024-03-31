551a4195972e449cc65f3d2d18cc844804c08efe
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-03-31 17:00:51 -0300

    schedule page

--- src/pages/agendar-consulta.tsx
+++ src/pages/agendar-consulta.tsx
@@ -1,16 +1,20 @@
+import { Container } from '../../styles/pages/Schedule'
 import { DetailBanner } from '../components/DetailBanner'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
+import { ScheduleForm } from '../components/ScheduleForm'
 
 export default function ScheduleConsult() {
   return (
     <>
-      <Header />
+      <Header homeActive={false} />
       <DetailBanner
         title="Agendar Consulta"
         description="Recupere seus pokémons em 5 segundos"
       />
-
+      <Container>
+        <ScheduleForm />
+      </Container>
       <Footer />
     </>
   )
---
+++ styles/pages/Schedule.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  padding-top: 2rem;
+  max-width: 1200px;
+  margin: 0 auto 3rem;
+`
