bd3963865b493a0f97dcfb27afd717b9cf185cec
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-04-01 23:34:59 -0300

    header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,13 +9,15 @@ interface Props {
 export function Header({ homeActive = false }: Props) {
   return (
     <S.Container>
-      <S.HomeLink href={'#'} data-expanded={homeActive} data-testid="homeLink">
+      <S.HomeLink href={'/'} data-expanded={homeActive} data-testid="homeLink">
         <Image src={PokeballIcon} alt="pokeball" width={37} height={37} />
         <span>Centro Pokémon</span>
       </S.HomeLink>
       <S.Navigation>
-        <S.OutlineLink href={'#'}>Quem somos</S.OutlineLink>
-        <S.ScheduleLink href={'#'}>Agendar Consulta</S.ScheduleLink>
+        <S.OutlineLink href={'/quem-somos'}>Quem somos</S.OutlineLink>
+        <S.ScheduleLink href={'/agendar-consulta'}>
+          Agendar Consulta
+        </S.ScheduleLink>
       </S.Navigation>
     </S.Container>
   )
